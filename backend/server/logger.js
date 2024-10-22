90f6dd323a73cd4a0aa9c4e8ecc7bc1779c66273
aayush0325 <96649223+aayush0325@users.noreply.github.com>
2024-10-22 13:55:52 +0530

    fix: changes in logging setup for better look at errors and prod

--- backend/server/logger.js
+++ backend/server/logger.js
@@ -1,4 +1,9 @@
 const { createLogger, format, transports } = require('winston');
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Determine if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
 
 const logger = createLogger({
   level: 'info',
@@ -9,9 +14,16 @@ const logger = createLogger({
     })
   ),
   transports: [
-    new transports.Console(), 
-    new transports.File({ filename: './logs/app.log' })
-  ]
+    // Conditional console transport
+    ...(isProduction ? [] : [new transports.Console()]), // Only log to console in non-production environments
+
+    new transports.File({ filename: './logs/app.log' }),
+
+    new transports.File({
+      filename: './logs/error.log',
+      level: 'error',
+    }),
+  ],
 });
 
 module.exports = logger;
