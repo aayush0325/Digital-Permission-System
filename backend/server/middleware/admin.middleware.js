831db5593b2e8caa676f815c48a6b6a3d35ecde9
aayush0325 <96649223+aayush0325@users.noreply.github.com>
2024-10-22 13:40:18 +0530

    feat: logging added in services and middlewares

--- backend/server/middleware/admin.middleware.js
+++ backend/server/middleware/admin.middleware.js
@@ -1,3 +1,5 @@
+const logger = require('../logger'); // Import Winston logger
+
 const adminEmails = [
   "doaa@iitbhu.ac.in",
   "dosa@iitbhu.ac.in",
@@ -6,12 +8,15 @@ const adminEmails = [
 
 const adminCheckMiddleware = (req, res, next) => {
   if (!req.isAuthenticated() || !req.user) {
+    logger.warn('Unauthorized access attempt - user is not authenticated');
     return res.status(401).json({ message: "Unauthorized" });
   }
 
   if (adminEmails.includes(req.user.email)) {
+    logger.info(`Admin access granted to ${req.user.email}`);
     next();
   } else {
+    logger.warn(`Unauthorized access attempt by ${req.user.email}`);
     res.status(403).json({ message: "Unauthorized" });
   }
 };
